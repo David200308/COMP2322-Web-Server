bdcf1a31cbcf3cf3be888baf735508fc1a196e7e
David Jiang <jiangguanlin2003@outlook.com>
2023-03-09 09:15:59 +0800

    Add the portCanUse function

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# COMP2322-Web-Server
+# COMP2322 Web Server
+
 A COMP2322 Project - Web Server
+
+## Programming Language
+
+- Python 3
+
+## Member
+
+- Guanlin Jiang
---
+++ port.py
@@ -0,0 +1,17 @@
+import socket
+
+def portCanUse(hostName, port):
+    '''
+        Checking the port is used or not, if used +1 and recheck, if not return and use it.
+    '''
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    if (sock.connect_ex((hostName, port)) == 0):
+        return portCanUse(hostName, port + 1)
+    else:
+        sock.close()
+        print("port can use", port)
+        return port
+
+# portCanUse('127.0.0.1', 1234)
